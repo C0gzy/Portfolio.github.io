fb7e8682227bcbc5a6e22f9af15ccb247796844e
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2025-01-22 20:12:07 +0000

    initial commit

---
+++ mainportfo/components/Images.jsx
@@ -0,0 +1,35 @@
+//Tech Images
+import ReactImage from '../Images/Tech/React.webp';
+import JSImage from '../Images/Tech/jsICON.webp';
+import PythonImage from '../Images/Tech/python_Icon.webp';
+import SQL from '../Images/Tech/SQLicon.png';
+import Csharp from '../Images/Tech/c-sharp-logo.png';
+import NodeJS from '../Images/Tech/nodejs-icon-logo.png';
+import Java from '../Images/Tech/javaicon.png';
+import firebase from '../Images/Tech/firebase.png';
+
+//Project Images
+import Plycs from '../Images/ProjectPhotos/Plycs.png';
+import TopHorror from '../Images/ProjectPhotos/TopHorror.png'
+import MarchSquare from '../Images/ProjectPhotos/MarchingSq.jpg';
+import Tower from '../Images/ProjectPhotos/TheTower.png';
+import MM from '../Images/Disc.png';
+
+const images= {
+    ReactImage,
+    JSImage,
+    PythonImage,
+    SQL,
+    Csharp,
+    NodeJS,
+    Java,
+    firebase,
+
+    Plycs,
+    TopHorror,
+    MarchSquare,
+    Tower,
+    MM
+}
+
+export default images;

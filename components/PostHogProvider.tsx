09a4c4633b31b2c8711596b63fad40339bd6922b
Tom Cogzell <tomcogzell8@gmail.com>
2025-11-05 11:45:36 +0000

    updated for static

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -7,11 +7,11 @@ import { useEffect } from "react"
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
+      api_host: "https://eu.i.posthog.com",
       ui_host: "https://eu.posthog.com",
       defaults: '2025-05-24',
       capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-      debug: process.env.NODE_ENV === "production",
+      debug: process.env.NODE_ENV !== "production",
     })
   }, [])
 

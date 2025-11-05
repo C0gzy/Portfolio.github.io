3db8cc8c89c9cbbaef9ee2a501feb8cd4ee29ab0
Tom Cogzell <tomcogzell8@gmail.com>
2025-11-05 11:23:57 +0000

    added posthog anayltics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Analytics } from "@vercel/analytics/next"
+import { PostHogProvider } from '../components/PostHogProvider'
 
 export const metadata: Metadata = {
   title: "Tom Cogzell's Portfolio",
@@ -17,7 +17,11 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/Icon.ico" />
       </head>
-      <body>{children}<Analytics /></body>
+      <body>
+        <PostHogProvider>
+          {children}
+        </PostHogProvider>
+      </body>
     </html>
   )
 }
